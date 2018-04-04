8fc658d1cffd3b0f7dd5ac17c18026aec422de4d
zahra falah <sepidehfallah2@gmail.com>
2018-04-03 20:17:53 -0700

    Mon

--- keys.js
+++ keys.js
@@ -1,13 +1,27 @@
 console.log('this is loaded');
+var twitter = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+// exports.twitter = {
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// };
 
-exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
+var spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET 
+}
+module.exports = {
+    twitter : twitter,
+    spotify : spotify
+}
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
--- liri.js
+++ liri.js
@@ -1,30 +1,45 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
 
 
 var Twitter = require('twitter');
-var spotify = require('spotify');
+var spotify = require('node-spotify-api');
 var request = require('request');
 var fs = require('fs');
 var keys = require('./keys.js');
 
 
-fs.appendFile('./log.txt', 'User Command: node liri.js my-tweets\n\n', (err) => {
-    if (err) throw err;
-});
-var client = new Twitter(twitterKeys);
-var params = {screen_name: 'sepidflh', count: 20};
+var cmdArgs = process.argv;
+var liriCommand = cmdArgs[2];
 
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(error);
-  }
-  else{
-    var outPut = 'User Tweets:';
-    for (var i = 0; i < tweets.length; i++) {
-        outPut += 'Created on: ' + tweets[i].created_at +'Tweet content: ' + tweets[i].text;
-    }
+function retrieveTweets() {
 
-  }
-});
+    fs.appendFile('./log.txt', 'User Command: node liri.js my-tweets\n\n', (err) => {
+            if (err) throw err;
+    });
+
+    var client = new Twitter(keys.twitter);
+    console.log(client);
+
+    var params = {screen_name: 'sepidflh', count: 20};
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (error) {
+                console.log(error);
+            }
+            else{
+                var outPut = 'User Tweets:';
+                for (var i = 0; i < tweets.length; i++) {
+                    outPut += 'Created on: '+ '-----------' + tweets[i].created_at + '-----------' + 'Tweet content: ' + tweets[i].text;
+                }
+
+                fs.appendFile('./log.txt', 'LIRI Response:\n\n' + outPut + '\n', (err) => {
+                if (err) throw err;
+                console.log(outPut);
+
+                })
+            }
+    })
+};
+retrieveTweets();
 
